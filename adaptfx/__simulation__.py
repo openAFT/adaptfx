5704438f8bbc762295921660f3fc798715c7aa46
Janic Weber <janic.weber@gmail.com>
2022-07-04 16:50:13 +0200

    initiate testing for number of fraction minimisation

--- adaptfx/__simulation__.py
+++ adaptfx/__simulation__.py
@@ -4,6 +4,14 @@ import numpy as np
 from scipy.interpolate import interp1d
 from scipy.stats import gamma, truncnorm
 
-[a, b] = intmin.data_fit(np.array([[0.99, 0.95, 0.98, 1.02], [0.95, 0.9, 0.8, 0.9]]))
-relay = intmin.whole_plan(4, [0.99, 0.95, 0.98, 0.96, 1.02], a, b, 50)
-print(relay)
+n_frac = 3
+[a, b] = intmin.data_fit(np.array([[0.99, 0.95, 0.98], [0.95, 0.9, 0.8]]))
+c_list = np.arange(1, 10, 1)**5
+dose_delivery = np.zeros((len(c_list),n_frac))
+for i, c in enumerate(c_list):
+    print(c)
+    relay = intmin.whole_plan(n_frac, [0.99, 0.95, 0.58, 0.96], a, b, 50, c)
+    relay2 = np.array(relay).transpose()[0][:]
+    dose_delivery[i] = relay2
+
+print(dose_delivery)
