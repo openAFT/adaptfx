b944943fcc67c6820dc997a415ead367cffc5a00
Janic Weber <janic.weber@uzh.ch>
2022-07-15 17:13:35 +0200

    change import for functionality

--- adaptfx_old/simulation.py
+++ adaptfx_old/simulation.py
@@ -1,5 +1,6 @@
 import numpy as np
-from interpol_frac import whole_plan
+import interpol2D_OARminfrac as intmin
+import interpol2D_OAR as intoar
 
 typ = 1
 n_frac = 8
@@ -7,10 +8,11 @@ n_frac = 8
 if typ==1:
     [a, b] = intmin.data_fit(np.array([[0.99, 0.95, 0.98], [0.95, 0.9, 0.8]]))
     sf = np.linspace(1, 1.1, n_frac)
+    print(a,b)
     c_list = np.linspace(0, 6, 6)
     dose_delivery = np.zeros((len(c_list),n_frac))
     for i, c in enumerate(c_list):
-        relay = whole_plan(n_frac, sf, a, b, 30, C=c, max_dose=100)
+        relay = intmin.whole_plan(n_frac, sf, a, b, 30, C=c, max_dose=100)
         relay2 = np.array(relay)[0][:]
         dose_delivery[i] = relay2
 
