9516482ed6d4ce5bb9611ec16d760a671049a1b8
Janic Weber <janic.weber@uzh.ch>
2022-07-16 14:35:31 +0200

    change parameter read in

--- adaptfx/__main__.py
+++ adaptfx/__main__.py
@@ -1,3 +1,4 @@
+from logging.config import dictConfig
 import numpy as np
 from reinforce import fraction_minimisation as frac
 from reinforce import oar_minimisation as oar
@@ -6,47 +7,84 @@ from reinforce import tumor_maximisation as tumor
 class RL_object():
     def __init__(self, algorithm, **params):
         self.algorithm = algorithm
-        parameters = {}
-        list = ['number_of_fractions',
-                'sparing_factors',
-                'alpha',
-                'beta',
-                'goal',
-                'C',
-                'bound_OAR',
-                'bound_tumor',
-                'BED_OAR',
-                'BED_tumor',
-                'OAR_limit',
-                'min_dose',
-                'max_dose',
-                'fixed_prob',
-                'fixed_mean',
-                'fixed_std',
 
-        ]
-
-        # for key in list:
-        #     if key in params:
-        #         parameters[key] = params['alpha']
-        #     else:
-        #         if self.algorithm=='frac' and  key in list_frac:
-        #             parameters[key] = params['alpha']
-        #         elif self.algorithm=='track' and key in list_track:
-        #             parameters[key] = params['alpha']
-        #         else:
-        #             print("key", key, "is missing in input")
+        dict = {'number_of_fractions':None,
+                'sparing_factors':None,
+                'alpha':None,
+                'beta':None,
+                'goal':None,
+                'C':None,
+                'bound_OAR':None,
+                'bound_tumor':None,
+                'BED_OAR':None,
+                'BED_tumor':None,
+                'OAR_limit':None,
+                'abt':10,
+                'abn':3,
+                'min_dose':0,
+                'max_dose':22.3,
+                'fixed_prob':0,
+                'fixed_mean':0,
+                'fixed_std':0,
+        }
 
         for key, value in params.items():
-            if key in list:
-                parameters[key] = value
+            if key in dict:
+                dict[key] = value
             else:
-                print("key", key, "is not allowed")
-        self.parameters = parameters
+                print("key '",key,"' is not allowed")
+
+        self.parameters = dict
 
     def optimise(self):
-        relay = frac.whole_plan(self.parameters)
-        print(relay)
+        params = self.parameters
+        if self.algorithm == 'oar':
+            relay = oar.whole_plan(number_of_fractions=params['number_of_fraction'],
+                                   sparing_factors=params['sparing_factors'],
+                                   alpha=params['alpha'],
+                                   beta=params['beta'],
+                                   goal=params['goal'],
+                                   abt=params['abt'],
+                                   abn=params['abn'],
+                                   min_dose=params['min_dose'],
+                                   max_dose=params['max_dose'],
+                                   fixed_prob=params['fixed_prob'],
+                                   fixed_mean=params['fixed_mean'],
+                                   fixed_std=params['fixed_std'],
+            )
+        elif self.algorithm == 'tumor':
+            relay = tumor.whole_plan(number_of_fractions=params['number_of_fraction'],
+                                    sparing_factors=params['sparing_factors'],
+                                    alpha=params['alpha'],
+                                    beta=params['beta'],
+                                    OAR_limit=params['OAR_limit'],
+                                    abt=params['abt'],
+                                    abn=params['abn'],
+                                    min_dose=params['min_dose'],
+                                    max_dose=params['max_dose'],
+                                    fixed_prob=params['fixed_prob'],
+                                    fixed_mean=params['fixed_mean'],
+                                    fixed_std=params['fixed_std'],
+            )
+        elif self.algorithm == 'frac':
+            relay = frac.whole_plan(number_of_fractions=params['number_of_fraction'],
+                                    sparing_factors=params['sparing_factors'],
+                                    alpha=params['alpha'],
+                                    beta=params['beta'],
+                                    goal=params['goal'],
+                                    C=params['C'],
+                                    abt=params['abt'],
+                                    abn=params['abn'],
+                                    min_dose=params['min_dose'],
+                                    max_dose=params['max_dose'],
+                                    fixed_prob=params['fixed_prob'],
+                                    fixed_mean=params['fixed_mean'],
+                                    fixed_std=params['fixed_std'],
+            )
+        else:
+            print("algorithm", self.algorithm, "not known")
+
+        return relay
 
 if __name__ == '__main__':
     rl_test = RL_object(algorithm='frac',
@@ -55,6 +93,7 @@ if __name__ == '__main__':
                         alpha=2.7,
                         beta=0.014,
                         goal=30,
-                        C=3
+                        C=3,
+                        p=2
     )
     # rl_test.optimise()
