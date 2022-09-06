94ac6506b72f0bd1fac0b81c1ce758ab67a99ccc
janicweber <janic.weber@uzh.ch>
2022-09-06 14:49:54 +0200

    add timing function to time general process

--- adaptfx/handler/aft_utils.py
+++ adaptfx/handler/aft_utils.py
@@ -1,11 +1,31 @@
 import time
 import handler.messages as m
+nme = __name__
 
-def timing_with_time():
-    start = time.perf_counter()
-    time.sleep(1)
-    stop = time.perf_counter()
-    return (stop - start)
+def timing(start=None):
+    """
+    measure time for general process:
+    for start: var_name = timing()
+    for strop: timing(var_name)
+
+    Parameters
+    ----------
+    start : time of starting function
+        the default is None.
+
+    Returns
+    -------
+    start : float
+        starting time
+        
+    """
+    if start == None:
+        start_time = time.perf_counter()
+        return start_time
+    else:
+        stop = time.perf_counter()
+        time_elapsed = (stop - start)
+        m.aft_message(f'process duration: {time_elapsed} s:', nme, 1)
 
 def key_reader(all_keys, full_dict, parameters, algorithm):
     """
