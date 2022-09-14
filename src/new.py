7266a74b362d1d8da1d0e7ebc9c14ebac8d43d15
janicweber <janic.weber@uzh.ch>
2022-09-14 16:18:31 +0200

    add work in progress for constant minimisation

--- src/new.py
+++ src/new.py
@@ -1,6 +1,8 @@
 import numpy as np
 import reinforce.plan as plan
 import scipy.optimize as opt
+# import scipy.interpolate as interpol
+import matplotlib.pyplot as plt
 
 def B_calc(algorithm, params):
     relay = plan.multiple(algorithm, params)
@@ -27,19 +29,32 @@ def B_n(n, param, reps):
             cumulative_dose[j] = B_calc('oar', param)[0]
         B[0][i] = np.mean(cumulative_dose)
         B[1][i] = np.std(cumulative_dose)
-
     return B
 
-n_target = 5
-fixed_mean = 0.9
-fixed_std = 0.04
+def f_n(n, c):
+    B = B_n(n, params, number_of_rep)[0]
+    C = C_n_linear(c, n)
+    return np.array((B + C, fractions(n)))
+
+def F_n(c, n, n_target):
+    fs = f_n(n, c)
+    min_index = np.argmin(fs[0])
+    n_min = fs[1][min_index]
+    print(fs)
+    print(n_min)
+    diff = np.abs(n_target - n_min)
+    return diff
+
+def F_n_fit(n, D):
+    bed = D * (1 + D/(n * params['abn']))
+    return C * n + bed
 
 params = {
             'number_of_fractions': 0,
             'sparing_factors': None,
             'fixed_prob': 1,
-            'fixed_mean': fixed_mean,
-            'fixed_std': fixed_std,
+            'fixed_mean': 0.9,
+            'fixed_std': 0.04,
             'tumor_goal': 30,
             'OAR_limit': 5,
             'C': None,
@@ -53,22 +68,17 @@ params = {
 
 N = 5
 C = 2
-number_of_rep = 8
+n_target = 5
+number_of_rep = 3
 
-def f_n(n, c):
-    B = B_n(n, params, number_of_rep)[0]
-    C = C_n_linear(c, n)
-    return np.array((B + C, fractions(n)))
+data = f_n(N, C)
 
-def F_n(c, n, n_target):
-    fs = f_n(n, c)
-    min_index = np.argmin(fs[0])
-    n_min = fs[1][min_index]
-    print(fs[0])
-    print(n_min)
-    diff = np.abs(n_target - n_min)
-    return diff
+popt, pcov = opt.curve_fit(F_n_fit, data[1], data[0], p0=(30))
+print(popt)
+
+plt.plot(fractions(N), F_n_fit(N, popt[0]))
+plt.show()
 
 # test = opt.minimize(F_n, [0], method='Nelder-Mead', args=(N, N-1))
-# print(test.x)
-print(F_n(1, 7, 5))
+
+# F_n(1, 7, 5)
