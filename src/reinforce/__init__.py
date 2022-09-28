425b1c7108692f4bbf4352c3df1c7634cbe459ed
janicweber <janic.weber@uzh.ch>
2022-09-28 12:41:41 +0200

    insert missing BED dose in module information

--- src/reinforce/__init__.py
+++ src/reinforce/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 this folder holds all reinforcement algorithms that includes:
--2D OAR BED minimisation, tumor BED maximisation and
+- 2D OAR BED minimisation, tumor BED maximisation and
 number of fraction minimisation
--3D algorithm that tracks OAR and tumor BED
+- 3D algorithm that tracks OAR BED and tumor BED
 
 Created on Wed Jul 13 13:28:51 2022
 
