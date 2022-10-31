5cce9ab346895fee906b5aaaea4a29d00a2d75a0
Janic Weber <janic.weber@uzh.ch>
2022-10-31 15:44:39 +0100

    move to src/adaptfx layout with single module

---
+++ src/adaptfx/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+this folder holds the adaptfx package
+Created on Wed Sep 7 15:28:02 2022
+@author: janicweber
+"""
+from .aft_messages import *
+from .aft_utils import *
+from .constants import *
+from .maths import *
+from .planning import *
+from .radiobiology import *
+from .reinforce_oar import *
+
+
