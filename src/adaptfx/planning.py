correct remaining bedt calculation and outside plot function

src/adaptfx/planning.py
@@ -37,6 +37,8 @@ def multiple(algorithm, keys, sets=afx.SETTING_DICT):
     first_tumor_dose = keys.accumulated_tumor_dose
     first_oar_dose = keys.accumulated_tumor_dose
 
+    policy_of_interest = None
+
     for i, keys.fraction in enumerate(fractions_list):
         keys.sparing_factors_public = keys.sparing_factors[0 : keys.fraction + 1]
         if algorithm == 'oar':
@@ -65,7 +67,8 @@ def multiple(algorithm, keys, sets=afx.SETTING_DICT):
         if sets.plot_policy == keys.fraction and sets.plot_policy !=0:
             # user specifies to plot policy number, if equal to fraction plot
             # if both zero than the user doesn't want to plot policy
-            afx.policy_plot(output.sf, output.states, output.policy, plot='True')
+            policy_of_interest = output
+            # afx.policy_plot(output.sf, output.states, output.policy, plot='True')
 
     exponent = afx.find_exponent(sets.dose_stepsize)
     physical, tumor, oar = np.around(
@@ -76,4 +79,4 @@ def multiple(algorithm, keys, sets=afx.SETTING_DICT):
     return [
             oar_sum, tumor_sum,
             np.vstack([physical, oar, tumor])
-            ]
+            ], policy_of_interest

src/adaptfx/reinforce.py
@@ -75,6 +75,7 @@ def min_oar_bed(keys, sets=afx.SETTING_DICT):
     # define number of bed_dose steps to fulfill stepsize
     bedt_states = np.linspace(accumulated_tumor_dose,
         tumor_goal, n_statesteps + 1)
+    remaining_states = bedt_states - accumulated_tumor_dose
     n_bedt_states = len(bedt_states)
     
     # relate actionspace to bed and possible sparing factors
@@ -177,10 +178,10 @@ def min_oar_bed(keys, sets=afx.SETTING_DICT):
 
     if finished:
         output = {'physical_dose': np.nan, 'tumor_dose': np.nan, 
-            'oar_dose': np.nan, 'sf': sf, 'states': bedt_states}
+            'oar_dose': np.nan, 'sf': sf, 'states': remaining_states}
     else:
         output = {'physical_dose': actionspace[action_index], 'tumor_dose': bedt_space[action_index], 
-            'oar_dose': bedn_space[action_index], 'sf': sf, 'states': bedt_states}
+            'oar_dose': bedn_space[action_index], 'sf': sf, 'states': remaining_states}
     if policy_plot:
         output['policy'] = policy
     return afx.DotDict(output)
@@ -258,6 +259,7 @@ def min_n_frac(keys, sets=afx.SETTING_DICT):
     # define number of bed_dose steps to fulfill stepsize
     bedt_states = np.linspace(accumulated_tumor_dose,
         tumor_goal, n_statesteps + 1)
+    remaining_states = bedt_states - accumulated_tumor_dose
     n_bedt_states = len(bedt_states)
     
     # relate actionspace to bed and possible sparing factors
@@ -362,10 +364,10 @@ def min_n_frac(keys, sets=afx.SETTING_DICT):
 
     if finished:
         output = {'physical_dose': np.nan, 'tumor_dose': np.nan, 
-            'oar_dose': np.nan, 'sf': sf, 'states': bedt_states}
+            'oar_dose': np.nan, 'sf': sf, 'states': remaining_states}
     else:
         output = {'physical_dose': actionspace[action_index], 'tumor_dose': bedt_space[action_index], 
-            'oar_dose': bedn_space[action_index], 'sf': sf, 'states': bedt_states}
+            'oar_dose': bedn_space[action_index], 'sf': sf, 'states': remaining_states}
     if policy_plot:
         output['policy'] = policy
     return afx.DotDict(output)