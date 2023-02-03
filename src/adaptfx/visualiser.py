3263557852c9a4e034b24a95eb7ff77d62f2300e
Janic Weber <janic.weber@uzh.ch>
2023-02-03 16:55:37 +0100

    remove numbering inside the grid

--- src/adaptfx/visualiser.py
+++ src/adaptfx/visualiser.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize as normalise
 import matplotlib.cm as cm
 
-def plot_val(sfs, states, policies, fractions):
+def plot_val(sfs, states, data, fractions, colmap='turbo'):
     """
     creates a subplot grid for the policy in each fraction that is given
 
@@ -14,8 +14,12 @@ def plot_val(sfs, states, policies, fractions):
         1d array with dimension n
     states : array
         1d array with dimension m
-    policies : array
+    data : array
         2d array with dimension n_policies:n:m
+    fractions : array
+        1d array with fractions numbers
+    colmap : string
+        heat colourmap for matplotlib
 
     Returns
     -------
@@ -23,10 +27,10 @@ def plot_val(sfs, states, policies, fractions):
         matplotlib pyplot figure
 
     """
-    [n_policies, _, _] = policies.shape
+    [n_grids, _, _] = data.shape
     # search for optimal rectangular size of subplot grid
-    n_rows = n_columns = int(np.sqrt(n_policies))
-    while n_rows * n_columns < n_policies:
+    n_rows = n_columns = int(np.sqrt(n_grids))
+    while n_rows * n_columns < n_grids:
         if n_rows >= n_columns:
             n_columns += 1
         else:
@@ -36,9 +40,9 @@ def plot_val(sfs, states, policies, fractions):
     x_min, x_max, y_min, y_max = sfs[0], sfs[-1], states[0], states[-1]
 
     # create shared colorbar
-    colmin, colmax = np.min(policies), np.max(policies)
+    colmin, colmax = np.min(data), np.max(data)
     normaliser = normalise(colmin, colmax)
-    colormap = cm.get_cmap('turbo')
+    colormap = cm.get_cmap(colmap)
     im = cm.ScalarMappable(cmap=colormap, norm=normaliser)
 
     # loop through the axes
@@ -47,19 +51,24 @@ def plot_val(sfs, states, policies, fractions):
     except:
         # in case ax is a 1x1 subplot
         axs = np.array([ax])
-        
-    for i, pol in enumerate(policies):
+    
+    for i, pol in enumerate(data):
         axs[i].imshow(pol, interpolation=None, origin='upper',
             norm=normaliser, cmap=colormap, aspect='auto',
             extent=[x_min, x_max, y_min, y_max])
-        axs[i].set_title(fractions[i])
+        axs[i].set_title(fractions[i], loc='left')
+        try: # get rid of inner axes values
+            axs[i].label_outer()
+        except:
+            pass
+
             
     fig.supxlabel('sparing factor')
     fig.supylabel('remaining BED')
     fig.tight_layout()
     fig.colorbar(mappable=im, ax=axs.tolist())
 
-    return fig
+    return axs
 
 def show_plot():
     plt.show()
