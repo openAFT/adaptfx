19bc4e9e9ff03bc88094c45fe25f567ed71925c3
Janic Weber <janicweber@users.noreply.github.com>
2023-03-22 14:47:58 +0100

    add __init__

---
+++ src/adaptsim/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+this folder holds the adaptsim package
+Created on Wed Jan 27 12:28:31 2023
+@author: janicweber
+"""
+from .constants import ALL_SIM_DICT, KEY_DICT_SIM, RCPARAMS
+from .visualiser import *
+from .visualiser_data import *
+
+__all__ = ['']
+
