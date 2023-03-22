f8d016545a19a177aa99c0f7c8853ed00390475a
Janic Weber <85884139+janicweber@users.noreply.github.com>
2023-03-22 19:54:41 +0100

    add MC_object

--- src/adaptsim/__init__.py
+++ src/adaptsim/__init__.py
@@ -5,6 +5,7 @@ Created on Wed Jan 27 12:28:31 2023
 @author: janicweber
 """
 from .constants import ALL_SIM_DICT, KEY_DICT_SIM, RCPARAMS
+from .ast import MC_object
 from .visualiser import *
 from .visualiser_data import *
 
